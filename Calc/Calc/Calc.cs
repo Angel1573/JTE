be593b114a48bc36420d438eb750663f4e904b74
Mariska <Angel_1573@live.nl>
2017-06-07 11:52:12 +0200

    Calculato to test Jenkins for C#

---
+++ Calc/Calc/Calc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    public class Calc
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Min(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multi(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Devide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Calc/UnitTestProject/UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        Calc.Calc calc = new Calc.Calc();
+
+        [TestMethod]
+        public void TestSum()
+        {
+            int expected = 5;
+            int result = calc.Sum(2, 3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMin()
+        {
+            int expected = 3;
+            int result = calc.Min(5, 2);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
